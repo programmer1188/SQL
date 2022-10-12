00040e0b41836e0512870717a583ba9802e0e951
programmer1188 <72691700+programmer1188@users.noreply.github.com>
2022-10-11 19:11:31 -0500

    Add files via upload

--- sams_lesson2_Retrieving_Data.sql
+++ sams_lesson2_Retrieving_Data.sql
@@ -54,10 +54,6 @@ SELECT * FROM customers;
 SELECT cust_id FROM customers;
 
 
-#Order Data
-
-ORDER BY - CLAUSE - Takes one or columns by which to sort the output data.
-
 #Sorting Retrieved Data
 SELECT prod_name FROM Products ORDER BY prod_name;
 
@@ -93,3 +89,6 @@ ORDER BY cust_id, order_date DESC;
 #3 Our fictitious store obviously prefers to sell more expensive items, and lots of them. Write a SQL statement to display the quantity and price (item_price) from the OrderItems table, sorted with the highest quantity and highest price first.
 SELECT quantity, item_price FROM orderitems 
 ORDER BY quantity DESC, item_price DESC;
+
+
+ 
