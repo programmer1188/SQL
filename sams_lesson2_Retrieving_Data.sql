c9aeef5273cb1899e31e1ef3740dce60099eda8b
programmer1188 <72691700+programmer1188@users.noreply.github.com>
2022-10-11 19:11:18 -0500

    Update sams_lesson2_Retrieving_Data.sql

--- sams_lesson2_Retrieving_Data.sql
+++ sams_lesson2_Retrieving_Data.sql
@@ -52,3 +52,44 @@ SELECT DISTINCT prod_id FROM orderitems;
 SELECT * FROM customers;
 */
 SELECT cust_id FROM customers;
+
+
+#Order Data
+
+ORDER BY - CLAUSE - Takes one or columns by which to sort the output data.
+
+#Sorting Retrieved Data
+SELECT prod_name FROM Products ORDER BY prod_name;
+
+#Sorting multiple columns
+SELECT prod_id, prod_price, prod_name 
+FROM products 
+ORDER BY prod_price, prod_name;
+
+#sort using column position
+#Here the ORDER BY uses the 2 and 3 column names from the select 
+SELECT prod_id, prod_price, prod_name
+FROM products
+ORDER BY 2,3;
+
+#Specify Sort Direction -> DESC - Descending order ASC -> ASCENDING ORDER
+SELECT prod_id, prod_price, prod_name 
+FROM products 
+ORDER BY prod_price DESC;
+
+#MOST EXPENSIVE PRODUCTS ARE SORTED FIRST then Product Name
+SELECT prod_id, prod_price, prod_name 
+FROM products 
+ORDER BY prod_price DESC, prod_name;
+
+#Challenges Lesson 3
+#1 Write a SQL statement to retrieve all customer names (cust_name) from the Customers table, and display the results sorted from Z to A.
+SELECT cust_name FROM customers ORDER BY cust_name DESC;
+
+#2 Write a SQL statement to retrieve customer id (cust_id) and order number (order_num) from the Orders table, and sort the results first by customer id, and then by order date in reverse chronological order.
+SELECT cust_id, order_num FROM orders
+ORDER BY cust_id, order_date DESC;
+
+#3 Our fictitious store obviously prefers to sell more expensive items, and lots of them. Write a SQL statement to display the quantity and price (item_price) from the OrderItems table, sorted with the highest quantity and highest price first.
+SELECT quantity, item_price FROM orderitems 
+ORDER BY quantity DESC, item_price DESC;
